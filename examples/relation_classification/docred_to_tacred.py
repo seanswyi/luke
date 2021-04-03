e6ed24650e6e10fe7fdbdb35409e6c81210865ce
seanswyi <seantuokc@gmail.com>
2021-04-03 16:00:34 +0900

    DocRED to TACRED.

---
+++ examples/relation_classification/docred_to_tacred.py
@@ -0,0 +1,163 @@
+import argparse
+from itertools import permutations
+import json
+import os
+
+from tqdm import tqdm
+
+
+def adjust_positions(mentions, text):
+    mentions = sorted(mentions, key=lambda x: x['sent_id'])
+
+    for idx, mention in enumerate(mentions):
+        if mention['sent_id'] == 0:
+            continue
+
+        prev_sent_lengths = sum([len(x) for x in text[:mention['sent_id']]])
+        adjusted_positions = [i + prev_sent_lengths for i in mention['pos']]
+
+        mentions[idx]['pos'] = adjusted_positions
+
+    return mentions
+
+
+def process(data_file):
+    """
+    Function to convert DocRED to TACRED format.
+
+    DocRED keys: vertexSet, labels, title, sents
+    TACRED keys: id, docid, relation, token, subj_start, subj_end, obj_start, obj_end \
+                   subj_type, obj_type, stanford_pos, stanford_ner, stanford_head, \
+                   stanford_deprel
+    """
+    with open(file=data_file) as f:
+        data = json.load(fp=f)
+
+    converted_data = []
+
+    progress_bar = tqdm(iterable=data, desc=f"Processing {data_file}", total=len(data))
+    for sample in progress_bar:
+        text = sample['sents']
+        entities = sample['vertexSet']
+        triplets = sample['labels']
+        title = sample['title']
+
+        entity_idxs = list(range(len(entities)))
+        all_entity_pairs = list(permutations(iterable=entity_idxs, r=2))
+
+        positive_pairs = [(x['h'], x['t']) for x in triplets]
+        negative_pairs = [pair for pair in all_entity_pairs if pair not in positive_pairs]
+
+        # Positive sample loop.
+        for triplet in triplets:
+            template = {'docid': '',
+                        'relation': '',
+                        'token': [],
+                        'subj_start': [],
+                        'subj_end': [],
+                        'obj_start': [],
+                        'obj_end': [],
+                        'subj_type': '',
+                        'obj_type': ''}
+
+            head = triplet['h']
+            tail = triplet['t']
+            relation = triplet['r']
+
+            head_entity_mentions = adjust_positions(mentions=entities[head], text=text)
+            tail_entity_mentions = adjust_positions(mentions=entities[tail], text=text)
+
+            subj_starts = [x['pos'][0] for x in head_entity_mentions]
+            subj_ends = [x['pos'][1] for x in head_entity_mentions]
+            obj_starts = [x['pos'][0] for x in tail_entity_mentions]
+            obj_ends = [x['pos'][1] for x in tail_entity_mentions]
+
+            subj_type = head_entity_mentions[0]['type']
+            obj_type = tail_entity_mentions[0]['type']
+
+            template['docid'] = title
+            template['relation'] = relation
+            template['token'] = sum(text, [])
+            template['subj_start'] = subj_starts
+            template['subj_end'] = subj_ends
+            template['obj_start'] = obj_starts
+            template['obj_end'] = obj_ends
+            template['subj_type'] = subj_type
+            template['obj_type'] = obj_type
+
+            converted_data.append(template)
+
+        for negative_pair in negative_pairs:
+            template = {'docid': '',
+                        'relation': '',
+                        'token': [],
+                        'subj_start': [],
+                        'subj_end': [],
+                        'obj_start': [],
+                        'obj_end': [],
+                        'subj_type': '',
+                        'obj_type': ''}
+
+            head = negative_pair[0]
+            tail = negative_pair[1]
+            relation = 'Na'
+
+            head_entity_mentions = adjust_positions(mentions=entities[head], text=text)
+            tail_entity_mentions = adjust_positions(mentions=entities[tail], text=text)
+
+            subj_starts = [x['pos'][0] for x in head_entity_mentions]
+            subj_ends = [x['pos'][1] for x in head_entity_mentions]
+            obj_starts = [x['pos'][0] for x in tail_entity_mentions]
+            obj_ends = [x['pos'][1] for x in tail_entity_mentions]
+
+            subj_type = head_entity_mentions[0]['type']
+            obj_type = tail_entity_mentions[0]['type']
+
+            template['docid'] = title
+            template['relation'] = relation
+            template['token'] = sum(text, [])
+            template['subj_start'] = subj_starts
+            template['subj_end'] = subj_ends
+            template['obj_start'] = obj_starts
+            template['obj_end'] = obj_ends
+            template['subj_type'] = subj_type
+            template['obj_type'] = obj_type
+
+            converted_data.append(template)
+
+    return converted_data
+
+
+def main(args):
+    if not os.path.exists(args.save_dir):
+        os.makedirs(args.save_dir, exist_ok=True)
+
+    train_file = os.path.join(args.docred_dir, args.train_file)
+    dev_file = os.path.join(args.docred_dir, args.dev_file)
+    test_file = os.path.join(args.docred_dir, args.test_file)
+
+    converted_train_data = process(train_file)
+    converted_dev_data = process(dev_file)
+
+    train_savefile = os.path.join(args.save_dir, 'train_tacred.json')
+    dev_savefile = os.path.join(args.save_dir, 'dev_tacred.json')
+
+    with open(file=train_savefile, mode='w') as f:
+        json.dump(obj=converted_train_data, fp=f, indent=2)
+
+    with open(file=dev_savefile, mode='w') as f:
+        json.dump(obj=converted_dev_data, fp=f, indent=2)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--docred_dir', default='/hdd1/seokwon/data/DocRED/original', type=str)
+    parser.add_argument('--train_file', default='train_annotated.json', type=str)
+    parser.add_argument('--dev_file', default='dev.json', type=str)
+    parser.add_argument('--test_file', default='test.json', type=str)
+    parser.add_argument('--save_dir', default='/hdd1/seokwon/data/DocRED/TACRED-style', type=str)
+
+    args = parser.parse_args()
+
+    main(args)
--- examples/relation_classification/utils.py
+++ examples/relation_classification/utils.py
@@ -4,8 +4,9 @@ import os
 from tqdm import tqdm
 from transformers.tokenization_roberta import RobertaTokenizer
 
-HEAD_TOKEN = "[HEAD]"
-TAIL_TOKEN = "[TAIL]"
+
+HEAD_TOKEN = '[HEAD]'
+TAIL_TOKEN = '[TAIL]'
 
 
 class InputExample(object):
@@ -43,26 +44,26 @@ class InputFeatures(object):
 
 class DatasetProcessor(object):
     def get_train_examples(self, data_dir):
-        return self._create_examples(data_dir, "train")
+        return self._create_examples(data_dir, 'train')
 
     def get_dev_examples(self, data_dir):
-        return self._create_examples(data_dir, "dev")
+        return self._create_examples(data_dir, 'dev')
 
     def get_test_examples(self, data_dir):
-        return self._create_examples(data_dir, "test")
+        return self._create_examples(data_dir, 'test')
 
     def get_label_list(self, data_dir):
         labels = set()
         for example in self.get_train_examples(data_dir):
             labels.add(example.label)
-        labels.discard("no_relation")
-        return ["no_relation"] + sorted(labels)
+        labels.discard('no_relation')
+        return ['no_relation'] + sorted(labels)
 
     def _create_examples(self, data_dir, set_type):
         """
         Creates individual samples and stores them in InputExample objects.
         """
-        with open(os.path.join(data_dir, set_type + ".json"), "r") as f:
+        with open(file=os.path.join(data_dir, set_type + '.json')) as f:
             data = json.load(f)
 
         examples = []
@@ -114,7 +115,7 @@ class DatasetProcessor(object):
         return examples
 
 
-def convert_examples_to_features(examples, label_list, tokenizer, max_mention_length):
+def convert_examples_to_features(examples, label_list, tokenizer, max_mention_length, setting='sentence'):
     label_map = {l: i for i, l in enumerate(label_list)}
 
     def tokenize(text):
@@ -124,42 +125,46 @@ def convert_examples_to_features(examples, label_list, tokenizer, max_mention_le
             return tokenizer.tokenize(text)
 
     features = []
+    import pdb; pdb.set_trace()
     for example in tqdm(examples):
-        if example.span_a[1] < example.span_b[1]:
-            span_order = ("span_a", "span_b")
-        else:
-            span_order = ("span_b", "span_a")
+        if setting == 'sentence':
+            if example.span_a[1] < example.span_b[1]:
+                span_order = ("span_a", "span_b")
+            else:
+                span_order = ("span_b", "span_a")
 
-        tokens = [tokenizer.cls_token]
-        cur = 0
-        token_spans = {}
-        for span_name in span_order:
-            span = getattr(example, span_name)
-            tokens += tokenize(example.text[cur : span[0]])
-            start = len(tokens)
-            tokens.append(HEAD_TOKEN if span_name == "span_a" else TAIL_TOKEN)
-            tokens += tokenize(example.text[span[0] : span[1]])
-            tokens.append(HEAD_TOKEN if span_name == "span_a" else TAIL_TOKEN)
-            token_spans[span_name] = (start, len(tokens))
-            cur = span[1]
+            tokens = [tokenizer.cls_token]
+            cur = 0
+            token_spans = {}
+            for span_name in span_order:
+                span = getattr(example, span_name)
+                tokens += tokenize(example.text[cur : span[0]])
+                start = len(tokens)
+                tokens.append(HEAD_TOKEN if span_name == "span_a" else TAIL_TOKEN)
+                tokens += tokenize(example.text[span[0] : span[1]])
+                tokens.append(HEAD_TOKEN if span_name == "span_a" else TAIL_TOKEN)
+                token_spans[span_name] = (start, len(tokens))
+                cur = span[1]
 
-        tokens += tokenize(example.text[cur:])
-        tokens.append(tokenizer.sep_token)
+            tokens += tokenize(example.text[cur:])
+            tokens.append(tokenizer.sep_token)
 
-        word_ids = tokenizer.convert_tokens_to_ids(tokens)
-        word_attention_mask = [1] * len(tokens)
-        word_segment_ids = [0] * len(tokens)
+            word_ids = tokenizer.convert_tokens_to_ids(tokens)
+            word_attention_mask = [1] * len(tokens)
+            word_segment_ids = [0] * len(tokens)
 
-        entity_ids = [1, 2]
-        entity_position_ids = []
-        for span_name in ("span_a", "span_b"):
-            span = token_spans[span_name]
-            position_ids = list(range(span[0], span[1]))[:max_mention_length]
-            position_ids += [-1] * (max_mention_length - span[1] + span[0])
-            entity_position_ids.append(position_ids)
+            entity_ids = [1, 2]
+            entity_position_ids = []
+            for span_name in ("span_a", "span_b"):
+                span = token_spans[span_name]
+                position_ids = list(range(span[0], span[1]))[:max_mention_length]
+                position_ids += [-1] * (max_mention_length - span[1] + span[0])
+                entity_position_ids.append(position_ids)
 
-        entity_segment_ids = [0, 0]
-        entity_attention_mask = [1, 1]
+            entity_segment_ids = [0, 0]
+            entity_attention_mask = [1, 1]
+        elif setting == 'document':
+            pass
 
         features.append(
             InputFeatures(
